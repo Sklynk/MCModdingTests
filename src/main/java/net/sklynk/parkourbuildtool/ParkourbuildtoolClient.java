adbf72b709f69813fc511da8563cb5c9bfd61711
Sklynk <Oliboyum627@gmail.com>
2024-10-17 18:28:11 -0500

    Initial commit

---
+++ src/main/java/net/sklynk/parkourbuildtool/ParkourbuildtoolClient.java
@@ -0,0 +1,10 @@
+package net.sklynk.parkourbuildtool;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ParkourbuildtoolClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
